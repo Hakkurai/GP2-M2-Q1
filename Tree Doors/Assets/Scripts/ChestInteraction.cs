d9d091533181ba4fbd1350286fec85825d6334c7
Hakkurai <gparagas2003@gmail.com>
2025-02-05 10:24:29 -0800

    added interaction mechanic

---
+++ Tree Doors/Assets/Scripts/ChestInteraction.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class ChestInteraction : MonoBehaviour
+{
+    public GameObject[] doors;  // Assign the 3 doors in the Inspector
+    private bool isPlayerNearby = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNearby = true;
+            Debug.Log("Player is near the chest. Press 'E' to interact.");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNearby = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Player interacted with the chest.");
+            OpenDoors();
+        }
+    }
+
+    void OpenDoors()
+    {
+        foreach (GameObject door in doors)
+        {
+            if (door != null)
+            {
+                Debug.Log(door.name + " is now open!");
+            }
+        }
+    }
+}
