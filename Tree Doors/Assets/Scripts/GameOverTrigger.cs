93121dc8ca487e109f46702ca59dc64fc80a6b5f
Hakkurai <gparagas2003@gmail.com>
2025-02-10 07:57:15 +0800

    modified GameOverTrigger script + added new sounds

--- Tree Doors/Assets/Scripts/GameOverTrigger.cs
+++ Tree Doors/Assets/Scripts/GameOverTrigger.cs
@@ -6,14 +6,12 @@ public class GameOverTrigger : MonoBehaviour
     // This will detect the collision with the "Game Over" box collider
     void OnTriggerEnter(Collider other)
     {
-        // Check if the colliding object is the player (you can add more specific checks like tag if needed)
+        // Check if the colliding object is the player 
         if (other.CompareTag("Player"))
         {
-            // Output "Game Over" to the console
-            Debug.Log("Game Over! Returning to Main Level. . .");
 
-            // Load the main level (replace "MainLevel" with the actual scene name)
-            SceneManager.LoadScene("MainLevel"); // Make sure "MainLevel" matches your scene name in the build settings
+            // Load the GameOverScene 
+            SceneManager.LoadScene("GameOverScene");
         }
     }
 }
